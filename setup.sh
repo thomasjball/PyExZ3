0bab794454f8d03c05c539d6e838d777198e1997
algobardo <mezzetti@gmail.com>
2014-06-09 15:37:23 +0200

    MacOS with homebrew

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Homebrew default locations (both for python and z3)
+export PYTHONMODS="usr/local/lib/python2.7/site-packages:/Library/Python/2.7/site-packages:~/Library/Python/2.7/lib/python/site-packages" 
+export Z3HOME="/usr/local/Cellar/z3/4.3.1/lib/python2.7/site-packages/"
+export Z3BIN="/usr/local/Cellar/z3/4.3.1/bin"
+
+
+# Python setup
+export PYTHONPATH=$PYTHONMODS:$Z3HOME
+
+# Path setup
+export PATH=$PATH:$Z3BIN
+
+
