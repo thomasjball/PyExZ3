8b19c7726702028d8bea1b4cb26e4fa0a9120a7d
Thomas Ball <thomas.j.ball@outlook.com>
2014-06-30 10:14:02 -0700

    add back setup

---
+++ setup.bat
@@ -0,0 +1,6 @@
+set Z3HOME=c:\users\tball\documents\github\z3\z3\build
+set PYTHONHOME=c:\Python34
+set GRAPHVIZHOME="c:\Program Files (x86)\Graphviz2.38\bin"
+set PATH=%PATH%;%PYTHONHOME%;%Z3HOME%;%GRAPHVIZHOME%
+set PYTHONPATH=%PYTHONPATH%;%Z3HOME%
+
