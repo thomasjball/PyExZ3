355daca8808ff38fcf22d857f93680bababe034e
Tom Ball <thomas.j.ball@outlook.com>
2014-07-15 11:35:14 -0700

    slides

--- test/maxtest.py
+++ test/maxtest.py
@@ -1,15 +1,15 @@
 # Created by Thomas Ball (2014)
 #
 
-def max2(a,b):
-	if (a < b):
-		return b
+def max2(s,t):
+	if (s < t):
+		return t
 	else:
-		return a
+		return s
 
 
-def max4(a,b,c,d):
-	return max2(max2(a,b),max2(c,d))
+def max4(x,y,x2,y2):
+	return max2(max2(x,y),max2(x2,y2))
 
 
 def maxtest(a,b,c,d):
