f94d364ead82f8ba98135427271efa354be4837a
Tom Ball <thomas.j.ball@outlook.com>
2014-07-21 10:38:30 -0700

    fix from Oded Padon

--- run_tests.py
+++ run_tests.py
@@ -21,7 +21,7 @@ files = [ f for f in os.listdir(test_dir) if re.search(".py$",f) ]
 for f in files:
 	# execute the python runner for this test
 	full = test_dir + os.sep + f
-	ret = os.system("python sym_exec.py -l "+full+".log "+full+" > "+full+".out")
+	ret = os.system(sys.executable + " sym_exec.py -l "+full+".log "+full+" > "+full+".out")
 	if (ret == 0):
 		print("Test " + f + " passed.")
 	else:
