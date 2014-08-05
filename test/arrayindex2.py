b21fc1bb800528bfa382e7fe6970ac69b80330d8
Thomas Ball <thomas.j.ball@outlook.com>
2014-08-05 08:34:45 +0200

    update slides

--- test/arrayindex2.py
+++ test/arrayindex2.py
@@ -1,13 +1,11 @@
-A = [0, 1]
+A = [0, 1, 0, 0, 1, 0, 1]
 
 def arrayindex2(a):
- b = False 
- for x in A:
-   b = b or (A[x] and (a==x))
- if b:
-   return A[a]
+
+ if a in [ i for i in range(len(A)) if A[i] ]:
+   return a
  else:
    return "OTHER"
 
 def expected_result():
-  return [ 1, "OTHER" ]
+  return [ 1,4,6, "OTHER" ]
