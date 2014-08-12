a405a3f6ec386bbf68c4e90ec7f09e94f2c4f281
Thomas Ball <thomas.j.ball@outlook.com>
2014-08-12 19:48:44 +0200

    example that shows path coverage is missing

---
+++ examples/collatz.py
@@ -0,0 +1,11 @@
+def collatz(n):
+    if n <= 1:
+        return 1
+    else:
+        if n % 2 == 0:
+            return collatz(n / 2)
+        else:
+            return collatz(3 * n + 1)
+
+def expected_result():
+    return [1, 1, 1, 1]
