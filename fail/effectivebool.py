5a94cb52898072ebb5b66624e0a4930b13d32238
Peter Chapman <peter@cmu.edu>
2015-01-13 09:56:56 -0500

    Effectie Boolean Test Cleanup and Documentation

--- fail/effectivebool.py
+++ fail/effectivebool.py
@@ -1,14 +1,17 @@
+"""Tests strings and integers used as a branch condition. The
+interpreter calls the bool constructor with the contents of the branch
+condition passed in as the argument. If a __bool__ function does not
+exist, __len__ is called and compared to zero."""
 from symbolic.args import *
 
-@symbolic(s="foo", x=1)
-def effectivebool(s, x):
-	if s:
-		return 0
-	elif x:
-		return 1
-	else:
-	    return 2
+@symbolic(string="foo", num=1)
+def effectivebool(string, num):
+    if string:
+        return 0
+    elif num:
+        return 1
+    else:
+        return 2
 
 def expected_result():
-	return [0,1,2]
-    
+    return [0, 1, 2]
