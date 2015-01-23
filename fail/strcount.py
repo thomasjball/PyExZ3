9c6ee82ca0740ba9fbf7bfef02d80d703448b5ce
Peter Chapman <peter@cmu.edu>
2015-01-23 17:01:00 -0500

    String Count Test uses Strings of Length > 1

--- fail/strcount.py
+++ fail/strcount.py
@@ -4,8 +4,10 @@ from symbolic.args import *
 def strcount(s):
     if s.count("x") == 5:
         return 1
+    elif s.count("xy") == 5:
+        return 2
     else:
         return 0
 
 def expected_result():
-    return [0, 1]
+    return [0, 1, 2]
