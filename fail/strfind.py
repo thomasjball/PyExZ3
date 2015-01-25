27d3e74c28a9b6232db90d4d4e8e097af1fd03ff
Peter Chapman <peter@cmu.edu>
2015-01-25 13:50:42 -0500

    Added String Find Test Case

---
+++ fail/strfind.py
@@ -0,0 +1,15 @@
+from symbolic.args import *
+
+@symbolic(s="foo")
+def strfind(s):
+    find_idx = s.find("bar")
+	if find_idx == 15:
+		return 0
+	elif find_idx == -1:
+		return 1
+    else:
+        return 2
+
+def expected_result():
+	return [0, 1, 2]
+    
