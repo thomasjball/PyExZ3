e66a7897a65957512aa0b57f5697d6e2c548dc93
Peter Chapman <peter@cmu.edu>
2015-01-26 16:22:07 -0500

    Lowered the Constants in the String Count Test
    When the counts were 5, the tests were taking too long to run. There may
    be a bug in the solver where it has difficulty with the negation of
    string contains queries.

--- fail/strcount.py
+++ fail/strcount.py
@@ -1,13 +1,13 @@
 from symbolic.args import *
 
-@symbolic(s="x")
+@symbolic(s="foo")
 def strcount(s):
-    if s.count("x") == 5:
+    if s.count("x") == 2:
         return 1
-    elif s.count("xy") == 5:
+    elif s.count("xy") == 1:
         return 2
     else:
         return 0
 
-def expected_result():
-    return [0, 1, 2]
+def expected_result_set():
+    return {0, 1, 2}
