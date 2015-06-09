41b465cdb471d81da9f14c40e4054687da8d67dc
Peter Chapman <peter@cmu.edu>
2015-06-08 21:25:41 -0700

    String Count Mismatch with Empty String

--- test/cvc/strcount.py
+++ test/cvc/strcount.py
@@ -6,8 +6,10 @@ def strcount(s):
         return 1
     elif s.count("xy") == 1:
         return 2
+    elif s.count("") == 3:
+        return 3
     else:
         return 0
 
 def expected_result_set():
-    return {0, 1, 2}
+    return {0, 1, 2, 3}
