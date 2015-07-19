069c8e687e54ad4bdff4deeeee41fe2cdf7f583a
Peter Chapman <peter@cmu.edu>
2015-07-18 20:49:48 -0700

    Starting PyEx Code Refactor and Cleanup

--- pyexz3.py
+++ pyexz3.py
@@ -1,9 +1,6 @@
 # Copyright: see copyright.txt
 
-import os
-import sys
 import logging
-import traceback
 from optparse import OptionParser
 
 from symbolic.loader import *
@@ -19,49 +16,50 @@ parser = OptionParser(usage=usage)
 parser.add_option("-l", "--log", dest="logfile", action="store", help="Save log output to a file", default="")
 parser.add_option("-s", "--start", dest="entry", action="store", help="Specify entry point", default="")
 parser.add_option("-g", "--graph", dest="dot_graph", action="store_true", help="Generate a DOT graph of execution tree")
-parser.add_option("-m", "--max-iters", dest="max_iters", type="int", help="Run specified number of iterations", default=0)
+parser.add_option("-m", "--max-iters", dest="max_iters", type="int", help="Run specified number of iterations",
+                  default=0)
 parser.add_option("--cvc", dest="cvc", action="store_true", help="Use the CVC SMT solver instead of Z3", default=False)
 parser.add_option("--z3", dest="cvc", action="store_false", help="Use the Z3 SMT solver")
 
 (options, args) = parser.parse_args()
 
 if not (options.logfile == ""):
-	logging.basicConfig(filename=options.logfile,level=logging.DEBUG)
+    logging.basicConfig(filename=options.logfile, level=logging.DEBUG)
 
 if len(args) == 0 or not os.path.exists(args[0]):
-	parser.error("Missing app to execute")
-	sys.exit(1)
+    parser.error("Missing app to execute")
+    sys.exit(1)
 
 solver = "cvc" if options.cvc else "z3"
 
 filename = os.path.abspath(args[0])
-	
-# Get the object describing the application
-app = loaderFactory(filename,options.entry)
-if app == None:
-	sys.exit(1)
 
-print ("Exploring " + app.getFile() + "." + app.getEntry())
+# Get the object describing the application
+app = loaderFactory(filename, options.entry)
+if app is None:
+    sys.exit(1)
+
+print("Exploring " + app.getFile() + "." + app.getEntry())
 
 result = None
 try:
-	engine = ExplorationEngine(app.createInvocation(), solver=solver)
-	generatedInputs, returnVals, path = engine.explore(options.max_iters)
-	# check the result
-	result = app.executionComplete(returnVals)
+    engine = ExplorationEngine(app.createInvocation(), solver=solver)
+    generatedInputs, returnVals, path = engine.explore(options.max_iters)
+    # check the result
+    result = app.executionComplete(returnVals)
 
-	# output DOT graph
-	if (options.dot_graph):
-		file = open(filename+".dot","w")
-		file.write(path.toDot())	
-		file.close()
+    # output DOT graph
+    if options.dot_graph:
+        file = open(filename + ".dot", "w")
+        file.write(path.toDot())
+        file.close()
 
 except ImportError as e:
-	# createInvocation can raise this
-	logging.error(e)
-	sys.exit(1)
+    # createInvocation can raise this
+    logging.error(e)
+    sys.exit(1)
 
-if result == None or result == True:
-	sys.exit(0);
+if result is None or result:
+    sys.exit(0)
 else:
-	sys.exit(1);	
+    sys.exit(1)
