57969e26699eeec4657a690bc3e8b1528eef46d4
Chad Brewbaker <crb002@gmail.com>
2018-06-02 18:50:49 -0500

    Bumping to latest z3

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 # Homebrew default locations (both for python and z3)
 export PYTHONMODS="usr/local/lib/python2.7/site-packages:/Library/Python/2.7/site-packages:~/Library/Python/2.7/lib/python/site-packages" 
-export Z3HOME="/usr/local/Cellar/z3/4.3.1/lib/python2.7/site-packages/"
-export Z3BIN="/usr/local/Cellar/z3/4.3.1/bin"
+export Z3HOME="/usr/local/Cellar/z3/4.7.1/lib/python2.7/site-packages/"
+export Z3BIN="/usr/local/Cellar/z3/4.7.1/bin"
 
 
 # Python setup
